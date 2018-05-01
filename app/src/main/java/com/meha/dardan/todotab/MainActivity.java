2f217aa93dd73e1d322be58be913bedadec261ba
Dardan Meha <mdardan20@gmail.com>
2018-05-01 16:41:25 -0400

    inflate toolbar

--- app/src/main/java/com/meha/dardan/todotab/MainActivity.java
+++ app/src/main/java/com/meha/dardan/todotab/MainActivity.java
@@ -1,7 +1,9 @@
 package com.meha.dardan.todotab;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        // set text for each tab
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
+
+        // set tabs to fill entire layout
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
     }
 }
---
+++ app/src/main/java/com/meha/dardan/todotab/PagerAdapter.java
@@ -0,0 +1,31 @@
+package com.meha.dardan.todotab;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int mNumOfTabs;
+    public PagerAdapter(FragmentManager fm, int NumOfTabs){
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new TabFragment1();
+            case 1:
+                return new TabFragment2();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
